fe1908ec8e6aa6bae73cea90e6ef5943d8268bd6
Felippe-Goncalves-de-Souza <felippegoncalvesdesouza17@gmail.com>
2024-05-15 10:54:28 -0300

    Projeto finalizado 15/05/2024

--- src/js/script.js
+++ src/js/script.js
@@ -30,23 +30,25 @@ const botao_mes    = document.getElementById("botao_mes");
 botao_dia.addEventListener("click", ()=>{
     descelescionar_mes();
     descelescionar_semana();
-    work_dia.classList.add("visivel");
-    play_dia.classList.add("visivel");
-    estudo_dia.classList.add("visivel");
-    exercicio_dia.classList.add("visivel");
-    social_dia.classList.add("visivel");
-    autocuidado_dia.classList.add("visivel");
+    dia_visivel();
 })
 
 botao_semana.addEventListener("click", ()=>{
-    descelescionar_mes();
     descelescionar_dia();
+    descelescionar_mes();
     work_semana.classList.add("visivel");
     play_semana.classList.add("visivel");
     estudo_semana.classList.add("visivel");
     exercicio_semana.classList.add("visivel");
     social_semana.classList.add("visivel");
     autocuidado_semana.classList.add("visivel");
+
+    work_dia.classList.add("invisivel");
+    play_dia.classList.add("invisivel");
+    estudo_dia.classList.add("invisivel");
+    exercicio_dia.classList.add("invisivel");
+    social_dia.classList.add("invisivel");
+    autocuidado_dia.classList.add("invisivel");
 })
 
 botao_mes.addEventListener("click", ()=>{
@@ -58,6 +60,13 @@ botao_mes.addEventListener("click", ()=>{
     exercicio_mes.classList.add("visivel");
     social_mes.classList.add("visivel");
     autocuidado_mes.classList.add("visivel");
+
+    work_dia.classList.add("invisivel");
+    play_dia.classList.add("invisivel");
+    estudo_dia.classList.add("invisivel");
+    exercicio_dia.classList.add("invisivel");
+    social_dia.classList.add("invisivel");
+    autocuidado_dia.classList.add("invisivel");
 })
 
 function descelescionar_semana(){
@@ -76,6 +85,8 @@ function descelescionar_dia(){
     exercicio_dia.classList.remove("visivel");
     social_dia.classList.remove("visivel");
     autocuidado_dia.classList.remove("visivel");
+
+
   };
 
   function descelescionar_mes(){
@@ -86,3 +97,20 @@ function descelescionar_dia(){
     social_mes.classList.remove("visivel");
     autocuidado_mes.classList.remove("visivel");
   };
+
+   function dia_visivel(){
+    work_dia.classList.remove("invisivel");
+    play_dia.classList.remove("invisivel");
+    estudo_dia.classList.remove("invisivel");
+    exercicio_dia.classList.remove("invisivel");
+    social_dia.classList.remove("invisivel");
+    autocuidado_dia.classList.remove("invisivel");
+
+    work_dia.classList.add("visivel");
+    play_dia.classList.add("visivel");
+    estudo_dia.classList.add("visivel");
+    exercicio_dia.classList.add("visivel");
+    social_dia.classList.add("visivel");
+    autocuidado_dia.classList.add("visivel");
+
+   }
